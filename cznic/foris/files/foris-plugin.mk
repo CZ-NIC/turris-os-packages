a2cc7ce72edbb7deffbfb0b8dd651900f642120c
Stepan Henek <stepan.henek@nic.cz>
2017-04-07 16:46:33 +0200

    foris+plugins: guest wifi + make locale packages

--- cznic/foris/files/foris-plugin.mk
+++ cznic/foris/files/foris-plugin.mk
@@ -22,6 +22,38 @@ endef
 
 # $1 = package name
 # $2 = plugin name
+# $3 = plugin translation name
+define ForisPluginTranslation
+
+ define Package/$(1)-l10n-$(3)
+	TITLE:=$(1)-l10n-$(3)
+	DEPENDS:=\
+		+foris +foris-l10n-$(3)
+	MAINTAINER:=Stepan Henek <stepan.henek@nic.cz>
+ endef
+
+ define Package/$(1)-l10n-$(3)/install
+	$(INSTALL_DIR) $$(1)/$(FORIS_PLUGIN_DIR)/$(2)/locale/$(3)/LC_MESSAGES
+	$(CP) \
+		$$(PKG_BUILD_DIR)/src/locale/$(3)/LC_MESSAGES/*.mo \
+		$$(1)/$(FORIS_PLUGIN_DIR)/$(2)/locale/$(3)/LC_MESSAGES/
+ endef
+
+ define Package/$(1)-l10n-$(3)/postrm
+#!/bin/sh
+set -x
+/etc/init.d/lighttpd restart
+ endef
+
+ define Package/$(1)-l10n-$(3)/postinst
+#!/bin/sh
+set -x
+/etc/init.d/lighttpd restart
+ endef
+endef
+
+# $1 = package name
+# $2 = plugin name
 define ForisPlugin
  define Package/$(1)/install
 	$(INSTALL_DIR) $$(1)/$(FORIS_PLUGIN_DIR)/$(2)
@@ -30,7 +62,7 @@ define ForisPlugin
 		$(PKG_BUILD_DIR)/src/* \
 		$$(1)/$(FORIS_PLUGIN_DIR)/$(2)/
 
-	find $$(1) -name "*.po" -delete
+	$(RM) -r $$(1)/$(FORIS_PLUGIN_DIR)/$(2)/locale
 	$(RM) -r $$(1)/$(FORIS_PLUGIN_DIR)/$(2)/static/.sass-cache
 	$(RM) -r $$(1)/$(FORIS_PLUGIN_DIR)/$(2)/static/sass
  endef
