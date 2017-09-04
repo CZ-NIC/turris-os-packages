185fb611aef118d02ac37e60bfff63d6fc0cf61b
Stepan Henek <stepan.henek@nic.cz>
2017-09-04 10:53:37 +0200

    foris-controller: version bump + .mk script for modules creation added

---
+++ cznic/foris-controller/files/foris-controller-module.mk
@@ -0,0 +1,23 @@
+# $1 = package name
+
+define ForisControllerModule
+ define Package/$(1)/install
+	$(INSTALL_DIR) $$(1)$(PYTHON_PKG_DIR) $$(1)/usr/bin
+	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then find $(PKG_INSTALL_DIR)/usr/bin -mindepth 1 -maxdepth 1 -type f -exec $(CP) \{\} $$(1)/usr/bin/ \; ; fi
+	find $(PKG_INSTALL_DIR)$(PYTHON_PKG_DIR) -mindepth 1 -maxdepth 1 \( -type f -o -type d \) -exec $(CP) \{\} $$(1)$(PYTHON_PKG_DIR)/ \;
+	$(RM) $$(1)/usr/lib/python2.7/site-packages/foris_controller_modules/__init__.py
+	$(RM) $$(1)/usr/lib/python2.7/site-packages/foris_controller_backends/__init__.py
+ endef
+
+ define Package/$(1)/postrm
+#!/bin/sh
+set -x
+[ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/foris-controller restart
+ endef
+
+ define Package/$(1)/postinst
+#!/bin/sh
+set -x
+[ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/foris-controller restart
+ endef
+endef
