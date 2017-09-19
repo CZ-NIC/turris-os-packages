b17a3bf3ca6384ef812beac69c3290c75528b257
Martin Petráček <martin.petracek@nic.cz>
2017-09-19 15:12:44 +0200

    suricata-monitor: split trafic related stuff to pakon-light package

--- net/suricata-monitor/files/monitor.py
+++ net/suricata-monitor/files/monitor.py
@@ -125,57 +125,27 @@ if con:
                     'category text, signature text, hostname text)')
     except:
         logging.debug('Table "alerts" already exists')
-    try:
-        c.execute('CREATE TABLE traffic '
-                  '(start real primary key, duration integer, connections integer,'
-                  'src_mac text, src_ip text, src_port integer, dest_ip text, dest_port integer, '
-                  'proto text, app_proto text, bytes_send integer, '
-                  'bytes_received integer, app_hostname text, app_hostname_type integer)')
-#app_hostname_type: 0 - unknown, 1 - tls/http(app level), 2 - dns, 3 - reverse lookup
-    except:
-        logging.debug('Table "traffic" already exists')
-    try:
-        c.execute('CREATE TABLE dns '
-                  '(time integer, client text, name text, type text, data text)')
-    except:
-        logging.debug('Table "dns" already exists')
-    try:
-        c.execute('CREATE TABLE settings '
-                  '(key text, value integer)')
-        c.execute('INSERT INTO settings VALUES (?, ?)', ('db_schema_version', 1))
-    except:
-        logging.debug('Table "settings" already exists')
 
 # Main loop
 
 def exit_gracefully(signum, frame):
-    global c, con, active_flows
-    conntrack.terminate()
-    time.sleep(1)
-    if not con:
-        return
-    for flow in active_flows.itervalues():
-         c.execute('INSERT INTO traffic VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (flow[0], int(time.time()-flow[0]), 1, flow[1], flow[2], flow[3], flow[4], flow[5], flow[6], flow[7], 0, 0, flow[8], 1))
-    con.commit()
+    global server, con
     if con:
          con.close()
-    conntrack.kill()
+    server.close()
     sys.exit(0)
+
 
 signal.signal(signal.SIGINT, exit_gracefully)
 signal.signal(signal.SIGTERM, exit_gracefully)
-conntrack=None
 
-devnull = open(os.devnull, 'w')
+if os.path.exists( "/var/run/suricata_monitor.sock" ):
+    os.remove( "/var/run/suricata_monitor.sock" )
 
-active_flows={}
+logging.debug("Opening socket...")
 
-try:
-    conntrack = subprocess.Popen(["/usr/bin/python3","/usr/bin/suricata_conntrack_flows.py","/var/run/suricata_monitor.sock"], shell=False, stdout=subprocess.PIPE, stderr=devnull)
-except Exception as e:
-    logging.error("Can't run flows_conntrack.py")
-    logging.error(e)
-    sys.exit(1)
+server = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+server.bind("/var/run/suricata_monitor.sock")
 
 logging.debug("Listening...")
 
@@ -183,9 +153,9 @@ logging.debug("Listening...")
 while True:
     try:
         logging.debug('Getting data...')
-        line = conntrack.stdout.readline()
+        line = server.recv(4092)
         if not line:
-            break
+            continue
         line = string.strip(line)
         logging.debug(line)
         if not line:
@@ -226,73 +196,6 @@ while True:
                                data['alert']['category'], data['alert']['signature'],
                                data['hostname']))
 
-        # Handle all other traffic
-        if data['event_type'] == 'dns' and con:
-            logging.debug('Got dns!')
-            if data['dns'] and data['dns']['type'] == 'answer' and 'rrtype' in data['dns'].keys() and data['dns']['rrtype'] in ('A', 'AAAA', 'CNAME') and con:
-                c.execute('SELECT data FROM dns WHERE client = ? AND name = ? ORDER BY time LIMIT 1',
-                          (data['dest_ip'], data['dns']['rrname']))
-                row = c.fetchone()
-                if row is None or row[0] != data['dns']['rdata']:
-                    logging.debug('Saving DNS data')
-                    if row:
-                        logging.debug(' -> ' + row[0] + ' != ' + data['dns']['rdata'])
-                    c.execute('INSERT INTO dns VALUES (?,?,?,?,?)',
-                              (timestamp2unixtime(data['timestamp']),
-                              data['dest_ip'], data['dns']['rrname'], data['dns']['rrtype'],
-                              data['dns']['rdata']))
-
-        # Insert or update flow - it might already exist from TLS connection
-        if data['event_type'] == 'flow' and data['flow'] and con and data['proto'] in ['TCP', 'UDP']:
-            logging.debug('Got flow!')
-            if 'app_proto' not in data.keys():
-                data['app_proto'] = 'unknown'
-            if data['app_proto'] not in ['failed', 'dns']:
-                if data['flow_id'] in active_flows.keys():
-                    hostname = active_flows[data['flow_id']][8]
-                    del active_flows[data['flow_id']]
-                    hostname_type = 1
-                else:
-                    hostname = ''
-                    hostname_type = 0
-                if 'src_port' not in data.keys():
-                    data['src_port'] = ''
-                if int(data['flow']['bytes_toserver'])==0 or int(data['flow']['bytes_toclient'])==0:
-                    continue
-                try:
-                    c.execute('INSERT INTO traffic VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                              (timestamp2unixtime(data['flow']['start']),
-                               int(timestamp2unixtime(data['flow']['end'])-timestamp2unixtime(data['flow']['start'])), 1, data['ether']['src'], data['src_ip'],
-                               data['src_port'], data['dest_ip'], data['dest_port'],
-                               data['proto'], data['app_proto'], data['flow']['bytes_toserver'],
-                               data['flow']['bytes_toclient'], hostname, hostname_type))
-                    if c.rowcount!=1:
-                        logging.error("Can't insert flow")
-                except Exception as e:
-                    print(e)
-
-        # Store TLS details of flow
-        if data['event_type'] == 'tls' and data['tls'] and con:
-            logging.debug('Got tls!')
-            hostname = ''
-            if 'sni' in data['tls'].keys():
-                hostname = data['tls']['sni']
-            elif 'subject' in data['tls'].keys():
-                hostname = data['tls']['subject']
-                #get only CN from suject
-                m = re.search('(?<=CN=)[^,]*', hostname)
-                if m:
-                     hostname = m.group(0)
-            if not hostname:
-                continue
-            active_flows[data['flow_id']]=(time.time(), data['ether']['src'], data['src_ip'], data['src_port'], data['dest_ip'], data['dest_port'], data['proto'], 'tls', hostname)
-
-        # Store HTTP details of flow
-        if data['event_type'] == 'http' and data['http'] and con:
-            if 'hostname' not in data['http'].keys():
-                continue
-            active_flows[data['flow_id']]=(time.time(), data['ether']['src'], data['src_ip'], data['src_port'],data['dest_ip'], data['dest_port'], data['proto'], 'http', data['http']['hostname'])
-
         # Commit everything
         if con:
             con.commit()
@@ -303,6 +206,3 @@ while True:
     except IOError as e:
         if e.errno != errno.EINTR:
             raise
-
-logging.error("End of data?")
-logging.error("This may mean that suricata_conntrack_flows.py doesn't exist/is broken...")
