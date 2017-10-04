c97fbdd84b246c56d19775dbb8a314bce383845e
Martin Petráček <martin.petracek@nic.cz>
2017-10-04 15:20:54 +0200

    pakon-light: more experiments...

---
+++ cznic/pakon-light/files/backup_sqlite.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+if [ $# -ne 2 ]; then
+	echo "usage: $0 src dst"
+	exit 1
+fi
+
+SRC=$1
+DST=$2
+DST_EXT=$(echo "$DST" | awk -F . '{print $NF}')
+TMP_FILE=$(mktemp)
+[ -f "$SRC" ] || exit 1
+sqlite3 $SRC ".backup '$TMP_FILE'" || { rm -f "$TMP_FILE"; exit 1; }
+if [[ $DST_EXT == "xz" ]]; then
+	xz -9 --stdout "$TMP_FILE" > "$DST.tmp"|| { rm -f "$TMP_FILE"; exit 1; }
+else
+	cp "$TMP_FILE" "$DST.tmp"
+fi
+mv "$DST.tmp" "$DST"
