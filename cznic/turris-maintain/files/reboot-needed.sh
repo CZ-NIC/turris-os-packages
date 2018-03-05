69e9859cd045e057347c148de70186616904f7ea
Stepan Henek <stepan.henek@nic.cz>
2018-03-05 12:56:52 +0100

    turris-maintain: send notification when reboot-need script is called

--- cznic/turris-maintain/files/reboot-needed.sh
+++ cznic/turris-maintain/files/reboot-needed.sh
@@ -4,3 +4,7 @@
 
 # existence of this file will be verified from other program
 touch /tmp/device-reboot-required
+
+if [ -x /usr/bin/foris-notify-wrapper ]; then
+	/usr/bin/foris-notify-wrapper -m maintain -a reboot_required "{}"
+fi
