54d40708840cd4794a82f9de2abb1d40e915193f
Karel Kočí <karel.koci@nic.cz>
2018-04-19 14:10:03 +0200

    Drop nuci and ucollect
    These project are deprecated so let's drop them in new major Turris OS
    version.

--- net/ucollect-fake/files/hotplug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-MD5=$(ip addr | sed -ne 's/.*inet6\? //p' | sed -e 's/\/.*//' | sort -u | md5sum| cut -f1 -d\ )
-PREVIOUS=$(cat /tmp/addresses-ucollect-fake.md5 || true)
-if [ "$MD5" != "$PREVIOUS" ] ; then
-	/etc/firewall.d/with_reload/99-ucollect-fake.fw
-	echo "$MD5" >/tmp/addresses-ucollect-fake.md5
-fi
