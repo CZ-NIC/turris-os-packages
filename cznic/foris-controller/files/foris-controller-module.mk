f27f935c8bb9e593630df4dcdbb8e35211529ac0
Stepan Henek <stepan.henek@nic.cz>
2018-08-10 14:41:41 +0200

    foris-controller: migrate to python3

--- cznic/foris-controller/files/foris-controller-module.mk
+++ cznic/foris-controller/files/foris-controller-module.mk
@@ -5,8 +5,8 @@ define ForisControllerModule
 	$(INSTALL_DIR) $$(1)$(PYTHON_PKG_DIR) $$(1)/usr/bin
 	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then find $(PKG_INSTALL_DIR)/usr/bin -mindepth 1 -maxdepth 1 -type f -exec $(CP) \{\} $$(1)/usr/bin/ \; ; fi
 	find $(PKG_INSTALL_DIR)$(PYTHON_PKG_DIR) -mindepth 1 -maxdepth 1 \( -type f -o -type d \) -exec $(CP) \{\} $$(1)$(PYTHON_PKG_DIR)/ \;
-	$(RM) $$(1)/usr/lib/python2.7/site-packages/foris_controller_modules/__init__.py
-	$(RM) $$(1)/usr/lib/python2.7/site-packages/foris_controller_backends/__init__.py
+	$(RM) $$(1)/usr/lib/python*/site-packages/foris_controller_modules/__init__.py
+	$(RM) $$(1)/usr/lib/python*/site-packages/foris_controller_backends/__init__.py
  endef
 
  define Package/$(1)/postrm
