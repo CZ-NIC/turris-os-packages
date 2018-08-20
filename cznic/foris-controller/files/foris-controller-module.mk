6f80e57830581db37b5a24ab6be2516c243b5de7
Stepan Henek <stepan.henek@nic.cz>
2018-08-20 22:39:52 +0200

    foris-controller: python3

--- cznic/foris-controller/files/foris-controller-module.mk
+++ cznic/foris-controller/files/foris-controller-module.mk
@@ -2,11 +2,11 @@
 
 define ForisControllerModule
  define Package/$(1)/install
-	$(INSTALL_DIR) $$(1)$(PYTHON_PKG_DIR) $$(1)/usr/bin
+	$(INSTALL_DIR) $$(1)$(PYTHON3_PKG_DIR) $$(1)/usr/bin
 	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then find $(PKG_INSTALL_DIR)/usr/bin -mindepth 1 -maxdepth 1 -type f -exec $(CP) \{\} $$(1)/usr/bin/ \; ; fi
-	find $(PKG_INSTALL_DIR)$(PYTHON_PKG_DIR) -mindepth 1 -maxdepth 1 \( -type f -o -type d \) -exec $(CP) \{\} $$(1)$(PYTHON_PKG_DIR)/ \;
-	$(RM) $$(1)/usr/lib/python2.7/site-packages/foris_controller_modules/__init__.py
-	$(RM) $$(1)/usr/lib/python2.7/site-packages/foris_controller_backends/__init__.py
+	find $(PKG_INSTALL_DIR)$(PYTHON3_PKG_DIR) -mindepth 1 -maxdepth 1 \( -type f -o -type d \) -exec $(CP) \{\} $$(1)$(PYTHON3_PKG_DIR)/ \;
+	$(RM) $$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/foris_controller_modules/__init__.py
+	$(RM) $$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/foris_controller_backends/__init__.py
  endef
 
  define Package/$(1)/postrm
