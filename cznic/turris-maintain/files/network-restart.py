56bc0ac8137479e7e37d7b1585d9709c39c1c936
Stepan Henek <stepan.henek@nic.cz>
2018-09-06 11:20:44 +0200

    turris-maintain: scripts to reboot router and restart network added

---
+++ cznic/turris-maintain/files/network-restart.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+import uci
+import time
+import json
+import subprocess
+
+TIME = 2000
+STEP = 200
+
+
+u = uci.Uci()
+
+
+def uci_get(config, section, option, default):
+    try:
+        val = u.get(config, section, option)
+    except uci.UciExceptionNotFound:
+        return default
+    return val
+
+
+bus = uci_get("foris_controller", "main", "bus", "ubus")
+
+if bus == "ubus":
+    path = uci_get("foris-controller", "ubus", "notification_path", "/var/run/ubus.sock")
+    from foris_controller.buses.ubus import UbusNotificationSender
+    sender = UbusNotificationSender(path)
+elif bus == "unix":
+    path = uci_get(
+        "foris-controller", "unix", "notification_path",
+        "/var/run/foris-controller-notifications.sock"
+    )
+    from foris_controller.buses.unix_socket import UnixSocketNotificationSender
+    sender = UnixSocketNotificationSender(path)
+
+ips = []
+# try to detect ips from uci
+ips += [e for e in uci_get("network", "wan", "ipaddr", "").split(" ") if e]
+ips += [e for e in uci_get("network", "wan", "ip6addr", "").split(" ") if e]
+ips += [e for e in uci_get("network", "lan", "ipaddr", "").split(" ") if e]
+ips += [e for e in uci_get("network", "lan", "ip6addr", "").split(" ") if e]
+
+# try to detect_ips from ubus
+for network in ["wan", "lan"]:
+    try:
+        output = subprocess.check_output(["ifstatus", network])
+        data = json.loads(output)
+        ips += [e["address"] for e in data["ipv4-address"]]
+        ips += [e["address"] for e in data["ipv6-address"]]
+    except (subprocess.CalledProcessError, ):
+        pass
+
+
+remains = TIME
+while remains > 0:
+    sender.notify("maintain", "network-restart", {"ips": list(set(ips)), "remains": remains})
+    time.sleep(float(STEP) / 1000)
+    remains -= STEP
+
+sender.notify("maintain", "network-restart", {"ips": ips, "remains": 0})
+
+subprocess.call(["/etc/init.d/network", "restart"])
