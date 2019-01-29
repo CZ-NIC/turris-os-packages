393afe4c4ed0928ca6366fc054759071ade53d40
Karel Kočí <karel.koci@nic.cz>
2019-01-29 16:16:10 +0100

    switch-branch: fix previous commit

--- cznic/switch-branch/files/updater.lua
+++ cznic/switch-branch/files/updater.lua
@@ -18,7 +18,7 @@ if uci then -- We need UCI for this
 	local mode = uci_cursor:get("updater", "turris", "mode")
 	if not mode or mode == "branch" then
 		local branch = uci_cursor:get("updater", "turris", "branch")
-		if branch and stable[branch] then
+		if branch and not stable[branch] then
 			Install("cznic-repo-keys-test", { priority = 40 })
 		end
 	end
