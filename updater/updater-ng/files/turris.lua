0f33bd43204228b7c2dc23771e2382a6b8f567ef
Karel Kočí <karel.koci@nic.cz>
2019-05-13 16:01:00 +0200

    updater-ng: add support for turris_lists boot argument
    This adds support for turris_lists boot argument. This is intended to be
    list of pkglists to be forcefully included. Multiple lists can be
    specified by separating them with comma.
    For example:
    turris_lists=nas,openvpn
    
    This also drops in pkglists already dropped minimal builds.

--- updater/updater-ng/files/turris.lua
+++ updater/updater-ng/files/turris.lua
@@ -20,17 +20,37 @@ end
 local mode = uci_cnf("mode", "branch") -- should we follow branch or version?
 local branch = uci_cnf("branch", "hbs") -- which branch to follow
 local version = uci_cnf("version", nil) -- which version to follow
-local lists = uci_cnf("pkglists", {}) -- what additional lists should we use
-minimal_builds = uci_cnf("use_minimal", "0") == "1" -- if packages-minimal should be used
-Export('minimal_builds')
+local pkglists = uci_cnf("pkglists", {}) -- what additional lists should we use
 
 -- Verify that we have sensible configuration
-if type(lists) == "string" then -- if there is single list then uci returns just a string
-	lists = {lists}
-end
 if mode == "version" and not version then
 	WARN("Mode configured to be 'version' but no version provided. Changing mode to 'branch' instead.")
 	mode = "branch"
+end
+
+-- Convert lists to set set of lists (ensures uniqueness)
+if type(pkglists) == "string" then -- if there is single list then uci returns just a string
+	pkglists = {pkglists}
+end
+local lists = {}
+for list in pairs(pkglists) do
+	lists[list] = true
+end
+
+-- Load lists forced by boot arguments
+if root_dir == "/" then
+	local cmdf = io.open("/proc/cmdline")
+	if cmdf then
+		for cmdarg in cmdf:read():gmatch('[^ ]+') do
+			local key, value = cmdarg:match('([^=]+)=(.*)')
+			if key == "turris_lists" then
+				for list in value:gmatch('[^,]+') do
+					lists[list] = true
+				end
+			end
+		end
+		cmdf:close()
+	end
 end
 
 -- Detect host board
@@ -70,12 +90,6 @@ local script_options = {
 Script(base_url .. "base.lua", script_options)
 
 -- Additional enabled distribution lists
-local exec_list = {} -- We want to run userlist only once even if it's defined multiple times
-for _, l in ipairs(lists) do
-	if exec_list[l] then
-		WARN("Turris package list '" .. l .. "' specified multiple times")
-	else
-		Script(base_url .. l .. ".lua", script_options)
-		exec_list[l] = true
-	end
+for l in pairs(lists) do
+	Script(base_url .. l .. ".lua", script_options)
 end
