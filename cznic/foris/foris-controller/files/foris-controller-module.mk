8f2e8709efd6816340ef63110c5ce62440d8f127
Karel Kočí <cynerd@email.cz>
2019-06-06 23:27:16 +0200

    foris: refactor foris packages to be really python packages
    This uses correct python install sections instead of replacing it with
    its own. The problem was that python-package.mk does a lot of "standard"
    fixes in that install and replacing it caused those fixes to be skipped.
    There might have been originally reason on why to not use default
    install but I don't see it now.
    
    This also cleans a lot of other stuff around. Because we now use
    python-package.mk provided install we use Py3Package install instead.
    The same paradigm is used for optional ForisControllerModule install
    section.
    
    This also drops unnecessary hash-bang at the beginning of all scripts.
    Those scripts are not run directly but instead sourced. It is bad
    practice to do so because those scripts do not include functions they
    might use (such as config_load and so on). Also note that in default
    script is interpreted as shell so it is all right even if it is run
    directly. This is just more cleaner solution.
    
    This also drops all trace sets from same scripts. Most of those were
    only calling service restart and that is pretty much useless to print
    trace for that. There is only one exception and that is postinst of
    controller-netboot-module. It is also doing UCI manipulation. This is
    invalid in its core because it disallows configuration to be used. I am
    going to move that change to separate oneshot package used to migrate
    that configuration in future. That means that those trace outputs should
    not be required anywhere.
    
    There is one hack introduced here. Build system is checking if it is not
    invoking some random Makefile by grepping it. In case of package they
    look for pattern: call BuildPackage. This commit moves package
    declaration from package it self to definition in included file. That
    means that build system would ignore such Makefile. This introduces
    comment with content matching pattern it looks for so it is not ignored.

--- cznic/foris/foris-controller/files/foris-controller-module.mk
+++ cznic/foris/foris-controller/files/foris-controller-module.mk
@@ -1,23 +1,34 @@
 # $1 = package name
-
 define ForisControllerModule
- define Package/$(1)/install
-	$(INSTALL_DIR) $$(1)$(PYTHON3_PKG_DIR) $$(1)/usr/bin
-	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then find $(PKG_INSTALL_DIR)/usr/bin -mindepth 1 -maxdepth 1 -type f -exec $(CP) \{\} $$(1)/usr/bin/ \; ; fi
-	find $(PKG_INSTALL_DIR)$(PYTHON3_PKG_DIR) -mindepth 1 -maxdepth 1 \( -type f -o -type d \) -exec $(CP) \{\} $$(1)$(PYTHON3_PKG_DIR)/ \;
+
+ ifndef ForisControllerModule/$(1)/install
+  ForisControllerModule/$(1)/install:=:
+ endif
+
+ define Py3Package/$(1)/install
+	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then \
+		$(INSTALL_DIR) $$(1)/usr/bin ; \
+		$(CP) $(PKG_INSTALL_DIR)/usr/bin/* $$(1)/usr/bin/ ; \
+	fi
 	$(RM) $$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/foris_controller_modules/__init__.py
 	$(RM) $$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/foris_controller_backends/__init__.py
+	$$(call ForisControllerModule/$(1)/install,$$(1))
  endef
 
- define Package/$(1)/postrm
-#!/bin/sh
-set -x
+ ifndef Package/$(1)/postrm
+  define Package/$(1)/postrm
 [ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/foris-controller restart
- endef
+  endef
+ endif
 
- define Package/$(1)/postinst
-#!/bin/sh
-set -x
+ ifndef Package/$(1)/postinst
+  define Package/$(1)/postinst
 [ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/foris-controller restart
- endef
+  endef
+ endif
+
+ $$(eval $$(call Py3Package,$(1)))
+ $$(eval $$(call BuildPackage,$(1)))
+ $$(eval $$(call BuildPackage,$(1)-src))
+
 endef
--- cznic/foris/foris/files/foris-plugin.mk
+++ cznic/foris/foris/files/foris-plugin.mk
@@ -12,58 +12,56 @@ define ForisPluginTranslation
 
  define Package/$(1)-l10n-$(3)/install
 	$(INSTALL_DIR) $$(1)$(PYTHON3_PKG_DIR)/foris_plugins/$(2)/locale/$(3)/LC_MESSAGES
-
 	$(CP) \
 		$$(PKG_BUILD_DIR)/foris_plugins/$(2)/locale/$(3)/LC_MESSAGES/*.mo \
 		$$(1)$(PYTHON3_PKG_DIR)/foris_plugins/$(2)/locale/$(3)/LC_MESSAGES/
  endef
 
  define Package/$(1)-l10n-$(3)/postrm
-#!/bin/sh
-set -x
 [ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/lighttpd restart
  endef
 
  define Package/$(1)-l10n-$(3)/postinst
-#!/bin/sh
-set -x
 [ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/lighttpd restart
  endef
+
+ $$(eval $$(call BuildPackage,$(1)-l10n-$(3)))
 endef
 
 # $1 = package name
 # $2 = plugin name
 define ForisPlugin
 
- define Build/Compile
-	$(call Build/Compile/Py3Mod,,install --prefix=/usr --root="$(PKG_INSTALL_DIR)")
- endef
+ ifndef ForisPlugin/$(1)/install
+  ForisPlugin/$(1)/install:=:
+ endif
 
- define Package/$(1)/install
-
-	$(INSTALL_DIR) $$(1)$(PYTHON3_PKG_DIR)
-
-	$(CP) \
-		$(PKG_INSTALL_DIR)$(PYTHON3_PKG_DIR)/* \
-		$$(1)$(PYTHON3_PKG_DIR)/
-
+ define Py3Package/$(1)/install
+	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then \
+		$(INSTALL_DIR) $$(1)/usr/bin ; \
+		$(CP) $(PKG_INSTALL_DIR)/usr/bin/* $$(1)/usr/bin/ ; \
+	fi
+	$(RM) $$(1)$(PYTHON3_PKG_DIR)/foris_plugins/__init__.py
 	$(RM) -r $$(1)$(PYTHON3_PKG_DIR)/foris_plugins/*/locale/
-	$(RM) -r $$(1)$(PYTHON3_PKG_DIR)/foris_plugins/__init__.py
-
+	$$(call ForisPlugin/$(1)/install,$$(1))
  endef
 
- define Package/$(1)/postrm
-#!/bin/sh
-set -x
+ ifndef Package/$(1)/postrm
+  define Package/$(1)/postrm
 [ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/lighttpd restart
- endef
+  endef
+ endif
 
- define Package/$(1)/postinst
-#!/bin/sh
-set -x
+ ifndef Package/$(1)/postinst
+  define Package/$(1)/postinst
 [ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/lighttpd restart
- endef
+  endef
+ endif
 
+ $$(eval $$(call Py3Package,$(1)))
+ $$(eval $$(call BuildPackage,$(1)))
+ $$(eval $$(call BuildPackage,$(1)-src))
+ $(foreach trans,$(FORIS_TRANSLATIONS),$(call ForisPluginTranslation,$(1),netmetr,$(trans)))
 
 endef
 
