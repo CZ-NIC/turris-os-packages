ea043e3af5a6db7c793979f379797a111b62b039
Karel Kočí <cynerd@email.cz>
2019-06-20 20:28:57 +0200

    foris-controller: do not include init files
    Removing files in install section has not effect because they are
    installed later by python install script. This instead uses filespec to
    instruct that script to do removal.

--- cznic/foris/foris-controller/files/foris-controller-module.mk
+++ cznic/foris/foris-controller/files/foris-controller-module.mk
@@ -5,13 +5,17 @@ define ForisControllerModule
   ForisControllerModule/$(1)/install:=:
  endif
 
+ define Py3Package/$(1)/filespec
++|$(PYTHON3_PKG_DIR)
+-|$(PYTHON3_PKG_DIR)/foris_controller_modules/__init__.py
+-|$(PYTHON3_PKG_DIR)/foris_controller_backends/__init__.py
+ endef
+
  define Py3Package/$(1)/install
 	if [ -d $(PKG_INSTALL_DIR)/usr/bin ]; then \
 		$(INSTALL_DIR) $$(1)/usr/bin ; \
 		$(CP) $(PKG_INSTALL_DIR)/usr/bin/* $$(1)/usr/bin/ ; \
 	fi
-	$(RM) $$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/foris_controller_modules/__init__.py
-	$(RM) $$(1)/usr/lib/python$(PYTHON3_VERSION)/site-packages/foris_controller_backends/__init__.py
 	$$(call ForisControllerModule/$(1)/install,$$(1))
  endef
 
