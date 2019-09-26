e35739cec1f00db1a74ae1b745a13c0a60560c83
Stepan Henek <stepan.henek@nic.cz>
2019-09-26 16:19:53 +0200

    follectd: fix neighbours count

--- cznic/follectd/files/neighbours.sh
+++ cznic/follectd/files/neighbours.sh
@@ -1,5 +1,5 @@
 HOSTNAME="${COLLECTD_HOSTNAME:-localhost}"
 INTERVAL="${COLLECTD_INTERVAL:-60}"
 
-VALUE="$(ip n | wc -l)"
+VALUE="$(ip n | cut -d " " -f5 | sort -u | grep ..:..:..:..:..:.. | wc -l)"
 echo "PUTVAL \"$HOSTNAME/exec-neighbours/gauge-neighbours\" interval=$INTERVAL N:$VALUE"
