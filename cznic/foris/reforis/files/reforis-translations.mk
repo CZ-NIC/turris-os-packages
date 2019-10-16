a8852cb49a0e20b34f7162da2964662b9a32adf4
Stepan Henek <stepan.henek@nic.cz>
2019-10-16 15:21:36 +0200

    reforis+friends: separate translations packages + add some dependencies

---
+++ cznic/foris/reforis/files/reforis-translations.mk
@@ -0,0 +1,31 @@
+REFORIS_TRANSLATIONS:=cs
+
+# $1 = translation name
+define ReForisTranslation
+
+ define Package/reforis-l10n-$(1)
+	TITLE:=reforis-l10n-$(1)
+	DEPENDS:=\
+		+reforis
+	MAINTAINER:=CZ.NIC <packaging@turris.cz>
+ endef
+
+ define Package/reforis-l10n-$(1)/install
+	$(INSTALL_DIR) $$(1)$(PYTHON3_PKG_DIR)/reforis/translations/$(3)/LC_MESSAGES
+	$(CP) \
+		$$(PKG_BUILD_DIR)reforis/translations/$(1)/LC_MESSAGES/*.mo \
+		$$(1)$(PYTHON3_PKG_DIR)/reforis/translations/$(1)/LC_MESSAGES/
+ endef
+
+ define Package/reforis-l10n-$(1)/postrm
+#!/bin/sh
+[ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/lighttpd restart
+ endef
+
+ define Package/reforis-l10n-$(1)/postinst
+#!/bin/sh
+[ -n "$$$${IPKG_INSTROOT}" ] || /etc/init.d/lighttpd restart
+ endef
+
+ $$(eval $$(call BuildPackage,reforis-l10n-$(1)))
+endef
