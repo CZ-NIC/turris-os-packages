85875c82210e740e54a51fc103f30c2f50865388
Karel Kočí <cynerd@email.cz>
2020-06-29 15:50:21 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add Gitlab CI to test merge requests

--- .gitlab-ci/base.sh
+++ .gitlab-ci/base.sh
@@ -1,7 +1,7 @@
 # Common job's base detection
 
-master_base="$(git merge-base --fork-point origin/master HEAD)"
-develop_base="$(git merge-base --fork-point origin/develop HEAD)"
+master_base="$(git merge-base origin/master HEAD)"
+develop_base="$(git merge-base origin/develop HEAD)"
 if git merge-base --is-ancestor "$master_base" "$develop_base"; then
 	# We are closer to develop than master so build on HBL
 	branch="hbl"
