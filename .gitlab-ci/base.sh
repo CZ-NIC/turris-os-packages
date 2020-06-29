4fbda6fb99ebc23962b118828dd6c3caa73c87f9
Karel Kočí <cynerd@email.cz>
2020-06-29 15:28:38 +0200

    fixup! fixup! Add Gitlab CI to test merge requests

--- .gitlab-ci/base.sh
+++ .gitlab-ci/base.sh
@@ -1,5 +1,7 @@
 # Common job's base detection
 
+git fetch
+
 master_base="$(git merge-base --fork-point master HEAD)"
 develop_base="$(git merge-base --fork-point develop HEAD)"
 if git merge-base --is-ancestor "$master_base" "$develop_base"; then
