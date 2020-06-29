201e747e04e2a90028ae2080383f2d950101b7bb
Martin Prudek <martin.prudek@nic.cz>
2020-06-29 16:21:36 +0200

    sentinel-proxy: add dependency for EULA and more info about it
    Dependency for EULA was added even proxy can run without it, but most of
    the time, you will have proxy with minipot and/or nikola. In the proxy
    package, there is a configuration file for each sentinel part.
    
    While at it add details where you can found EULA and how you can agree
    with it.

--- collect/sentinel/sentinel-proxy/files/sentinel.sh
+++ collect/sentinel/sentinel-proxy/files/sentinel.sh
@@ -3,7 +3,7 @@
 
 allowed_to_run() {
     local component_name="$1";
-    agreed_with_eula && component_enabled "${component_name}" && return 0
+    agreed_with_eula "${component_name}" && component_enabled "${component_name}" && return 0
     return 1
 }
 
@@ -21,12 +21,20 @@ component_enabled() (
 )
 
 agreed_with_eula() (
+    local component_name="$1";
     config_load sentinel
 
     local agreed_eula_version
     config_get agreed_eula_version main agreed_with_eula_version "0"
     [ "$agreed_eula_version" -le "0" ] && {
-        echo "Failed to start - not agreed with EULA" >&2
+        echo "Failed to start ${component_name} - not agreed with EULA.
+
+EULA could be found at /usr/share/sentinel-eula/ and you can
+agree with it either in ReForis data collect tab or using
+uci config:
+$uci set sentinel.main.agreed_with_eula_version=1
+
+EULA version may increase in time. See documentation for more details."
         return 1
     }
     return 0
