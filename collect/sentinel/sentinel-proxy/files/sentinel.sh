9d7926bfcf33d8dafc6d96aa1c7bb7fa50423cdf
Karel Kočí <cynerd@email.cz>
2020-07-10 17:20:18 +0200

    sentinel-proxy: tweak sentinel.sh
    This changes code little bit.
    It uses tabs instead of spaces. Most of our shell code uses tabs. It is
    not a huge issue but when I was at it it makes sense to flip it.
    Prints now do not state that "start" failed but rather print generic
    error. In other words: "Failed to start" is removed. The reason is that
    this is not used just in components doing start but rather in any
    case checking if component is enabled or not.
    The last tweak is that some unnecessary returns were removed and
    general code was tweak little bit to be more clean (such as replacement
    of multi-line string with cat<<EOF.

--- collect/sentinel/sentinel-proxy/files/sentinel.sh
+++ collect/sentinel/sentinel-proxy/files/sentinel.sh
@@ -1,41 +1,39 @@
 #!/bin/sh
 
-
 allowed_to_run() {
-    local component_name="$1";
-    agreed_with_eula "${component_name}" && component_enabled "${component_name}" && return 0
-    return 1
+	local component_name="$1";
+	agreed_with_eula "${component_name}" && component_enabled "${component_name}"
 }
 
 component_enabled() (
-    local component_name="$1";
-    config_load sentinel
+	local component_name="$1";
+	config_load sentinel
 
-    local enabled
-    config_get_bool enabled "${component_name}" enabled "0"
-    [ "$enabled" = "1" ] || {
-        echo "Failed to start - ${component_name} not enabled" >&2
-        return 1
-    }
-    return 0
+	local enabled
+	config_get_bool enabled "${component_name}" enabled "0"
+	[ "$enabled" = "1" ] || {
+		echo "Sentinel ${component_name} not enabled" >&2
+		return 1
+	}
 )
 
 agreed_with_eula() (
-    local component_name="$1";
-    config_load sentinel
+	local component_name="$1";
+	config_load sentinel
 
-    local agreed_eula_version
-    config_get agreed_eula_version main agreed_with_eula_version "0"
-    [ "$agreed_eula_version" -le "0" ] && {
-        echo "Failed to start ${component_name} - not agreed with EULA.
+	local agreed_eula_version
+	config_get agreed_eula_version main agreed_with_eula_version "0"
+	[ "$agreed_eula_version" -le "0" ] || return 0
+
+	cat >&2 <<EOF
+Not agreed with EULA.
 
 EULA could be found at /usr/share/sentinel-eula/ and you can
 agree with it either in ReForis data collect tab or using
 uci config:
-$uci set sentinel.main.agreed_with_eula_version=1
+uci set sentinel.main.agreed_with_eula_version=1
 
 EULA version may increase in time. See documentation for more details."
-        return 1
-    }
-    return 0
+EOF
+	return 1
 )
