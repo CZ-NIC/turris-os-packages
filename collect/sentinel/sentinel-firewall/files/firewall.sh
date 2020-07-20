465ba8222f952b5ae57c4355fb4a73d93a195d49
Karel Kočí <karel.koci@nic.cz>
2020-07-20 15:41:27 +0200

    sentinel-firewall: reorder firewall config on start
    This checks and reorders sentinel-firewall config on firewall start.
    This is not ideal as this reorder is not applied in firewall run when
    reorder happens and there seems to be no reliable way to request
    firewall reload from firewall script itself (would cause firewall
    nesting with unknown results).
    
    We also does not have to reorder in uci-defaults as new config sections
    are always appended so they are the last one, making that operation
    useless code.

--- collect/sentinel/sentinel-firewall/files/firewall.sh
+++ collect/sentinel/sentinel-firewall/files/firewall.sh
@@ -8,6 +8,18 @@
 #		option reload '1'
 
 
+# Make sure that we are last config.
+# We have to be last config to ensure that we can reliably insert general block
+# rules for dynamic firewall and other hacks.
+# This is not ideal as we won't apply it in this run but rather by subsequent one
+# but that is enough to ensure that we are last in the end.
+uci -q reorder "firewall.sentinel_firewall=$(uci show firewall | grep -cE '^firewall\.[^.]+=')"
+if [ -n "$(uci changes firewall.sentinel_firewall)" ]; then
+	uci commit firewall.sentinel_firewall
+	echo "Warning: Sentinel-firewall include reordered: another firewall reload suggested!"
+fi
+
+
 # Remove any existing rule
 # (firewall3 removes only rules in chains it knows so we have to do this to
 # potentially clean after ourselves)
