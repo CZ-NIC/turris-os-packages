ac54e769a1defa608ba5db105bb25074d46f7be0
Karel Kočí <karel.koci@nic.cz>
2020-11-16 13:28:29 +0100

    dhparam: remove this package
    This package is replaced by changes in foris-controller-openvpn that
    now uses turris-cagen instead.

--- admin/dhparam/files/generate_dh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -f /etc/dhparam/dh2048.pem.new
-openssl dhparam -out /etc/dhparam/dh2048.pem.new 2048 && \
-[ -f /etc/dhparam/dh2048.pem.new ] && \
-rm -f /etc/dhparam/dh2048.pem && \
-mv /etc/dhparam/dh2048.pem.new /etc/dhparam/dh2048.pem && \
-/etc/init.d/dhparam disable && \
-ln -sf /etc/dhparam/dh2048.pem /etc/dhparam/dh-default.pem && \
-rm -f /etc/init.d/dhparam
