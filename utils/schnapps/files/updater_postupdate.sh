a6b996b734277890bee0b67e18bfeaf92a1ab4bd
Karel Kočí <karel.koci@nic.cz>
2021-06-07 22:34:59 +0200

    schnapps: prevent execution on non-btrfs filesystems
    We check if we are running on current root but we have to also check if
    we are running on BTRFS because otherwise it makes no sense to run
    schnapps. Even if schnapps would be configured to work on some other
    filesystem in terms of updater integration that is kind of useless. The
    files are stored primarily on root and thus root is what we care about.

--- utils/schnapps/files/updater_postupdate.sh
+++ utils/schnapps/files/updater_postupdate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 [ "$ROOT_DIR" = "/" ] || exit 0
+[ "$(findmnt -nf -o FSTYPE /)" = "btrfs" ] || exit 0
 source /etc/os-release
 schnapps create -t post "Automatic post-update snapshot ($PRETTY_NAME)"
