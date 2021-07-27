7148fc07e6fcdaa7d363f441217140f206c8c02f
Martin Matějek <martin.matejek@nic.cz>
2021-07-27 18:15:40 +0200

    turris-maintain: remove options to set message bus
    Followup of commit c54286420397116998fc87906b3fc786a7c32670
    
    Set mqtt as the only message bus, because we no longer use another message
    buses on regular router setup.

--- utils/turris-maintain/files/reboot.py
+++ utils/turris-maintain/files/reboot.py
@@ -6,44 +6,28 @@ import re
 import subprocess
 import euci
 
+from foris_controller.buses.mqtt import MqttNotificationSender
+
 TIME = 3000
 STEP = 200
 
 
 with euci.EUci() as uci:
-    bus = uci.get("foris-controller", "main", "bus", default="ubus")
-
     controller_id = None
 
-    if bus == "ubus":
-        path = uci.get(
-            "foris-controller", "ubus", "notification_path",
-            default="/var/run/ubus.sock"
-        )
-        from foris_controller.buses.ubus import UbusNotificationSender
-        sender = UbusNotificationSender(path)
-    elif bus == "unix":
-        path = uci.get(
-            "foris-controller", "unix", "notification_path",
-            default="/var/run/foris-controller-notifications.sock"
-        )
-        from foris_controller.buses.unix_socket import UnixSocketNotificationSender
-        sender = UnixSocketNotificationSender(path)
-    elif bus == "mqtt":
-        host = uci.get("foris-controller", "mqtt", "host", default="localhost")
-        port = int(uci.get("foris-controller", "mqtt", "port", default=11883))
-        passwd_path = uci.get(
-            "foris-controller", "mqtt", "credentials_file", default="/etc/fosquitto/credentials.plain"
-        )
-        with open(passwd_path, "r") as f:
-            credentials = re.match(r"^([^:]+):(.*)$", f.readlines()[0][:-1]).groups()
-        try:
-            controller_id = subprocess.check_output(
-                ["crypto-wrapper", "serial-number"]).decode().strip()
-        except subprocess.CalledProcessError:
-            controller_id = None
-        from foris_controller.buses.mqtt import MqttNotificationSender
-        sender = MqttNotificationSender(host, port, credentials)
+    host = uci.get("foris-controller", "mqtt", "host", default="localhost")
+    port = int(uci.get("foris-controller", "mqtt", "port", default=11883))
+    passwd_path = uci.get(
+        "foris-controller", "mqtt", "credentials_file", default="/etc/fosquitto/credentials.plain"
+    )
+    with open(passwd_path, "r") as f:
+        credentials = re.match(r"^([^:]+):(.*)$", f.readlines()[0][:-1]).groups()
+    try:
+        controller_id = subprocess.check_output(
+            ["crypto-wrapper", "serial-number"]).decode().strip()
+    except subprocess.CalledProcessError:
+        controller_id = None
+    sender = MqttNotificationSender(host, port, credentials)
 
     ips = []
     # try to detect ips from uci
